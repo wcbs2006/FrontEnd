6e436241060c9e427f11f4bf7c3af93c5d594ba0
Wescley Christian <108875067+weschristi@users.noreply.github.com>
2023-03-27 12:32:39 -0300

    Add files via upload

---
+++ testes/arrays/script.js
@@ -0,0 +1,5 @@
+let objetos = Array('Cadeira', 'Garfo', 'Impressora');
+
+let objetosInput = document.getElementById('input')
+let add = document.getElementById('add')
+let ord = document.getElementById('ord')
