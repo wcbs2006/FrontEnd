24fbbbe532bb81f0c61372a8d4db7ec188be725b
Wescley Christian <108875067+weschristi@users.noreply.github.com>
2023-04-10 12:29:07 -0300

    Add files via upload

---
+++ scriptsTests.js
@@ -0,0 +1,57 @@
+// var matricula = 400 ;
+// var desconto = ((27.5/100)*matricula) ;
+// var valorFinal ;
+
+// function efetuarPagamento(matricula, desconto){
+//     valorFinal = matricula - desconto
+//     return valorFinal;
+// }
+
+// console.log(efetuarPagamento(matricula, desconto))
+
+// const users = ["Ana", "Fernando", "Rafael", "Clara", "Maria"]
+// const readline = require('readline')
+// const rl = readline.createInterface({
+//     input: process.stdin, output: process.stdout
+// })
+// rl.question('Coloque um nome: ', (name) => {
+//     users.push(name)
+//     console.log(users)
+//     rl.close()
+// })
+
+// const person = ["Felipe", 30, "São Paulo", "Masculino", "999990000"]
+
+// console.log("Número de telefone de",person[0],": ", person[4])
+// person.pop()
+// console.log(person)
+
+// const fruits = ["Little Apple", "Squish Orange", "Sweet Banana", "Just a Pear"]
+
+// let indexOfSearch = fruits.indexOf("Squish Orange", 3)
+// console.log(indexOfSearch)
+
+// fruits.shift()
+
+// console.log(fruits)
+
+// const jobs = ["Frontend", "Backend", "DevOps", "FullStack"]
+
+// jobs.sort()
+
+// console.log(jobs)
+
+
+// jobs.unshift("Robótica", "Design")
+
+// console.log(jobs)
+
+// let indexOf = jobs.indexOf("Frontend")
+// console.log(indexOf)
+
+// const colors = ["Red", "Green", "White", "Black"]
+
+// let join = colors.join(" - ")
+
+// console.log(join)
+
